bf4cc0cbdd8b5359d98a8e4702caad8a8d31159d
Emma Collier <emma@exegetic.biz>
2018-05-07 15:16:47 +0200

    Chapter 3 - Variables.

---
+++ variables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head> 
+		<title>Variables</title>
+	</head>
+	<body>
+	<?php
+
+	$var1 = 10;
+	echo $var1;
+
+	echo "<br/>";
+
+	$var1 = 100;
+	echo $var1;
+
+	echo "<br/>";
+
+	$Var2 = "Hello world";
+	echo $Var2;
+
+	?>
+	</body>
+</html>
