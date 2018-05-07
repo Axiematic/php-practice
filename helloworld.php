c131c8c4477761cb553bad6ba8ffbbf4c272e84f
Emma Collier <emma@exegetic.biz>
2018-05-07 13:25:45 +0200

    Added initial filesto repo.

---
+++ helloworld.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head> 
+		<title>Hello World</title>
+	</head>
+	<body>
+		<?php echo "Hello World!"; ?><br />  <!-- breaks onto new line--> <!--echo makes the output show online -->
+		<?php echo "Hello" . " World!"; ?><br />   <!-- concatinates two sepaarate words together-->
+		<?php echo 2 + 3; ?>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<p>This is some text.</p>
+
+<?php phpinfo(); ?>
+
+<p>This is some more text.</p>
