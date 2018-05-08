9aa6cc425e0f4c595b45c945c46a6fb11ad0b0a6
Emma Collier <emma@exegetic.biz>
2018-05-08 15:23:15 +0200

    Chapter 3 - Null and Empty.

---
+++ NULL.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head> 
+		<title>NULL</title>
+	</head>
+	<body>
+
+	<?php
+
+	$var1 = null;
+	$var2 = "";
+	?>
+
+	var1 null?: <?php echo is_null($var1); ?><br/>
+	var2 null?: <?php echo is_null($var2); ?><br/>
+	</body>
+</html>
