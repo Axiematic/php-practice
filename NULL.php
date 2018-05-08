4b8081dfc6eca600a83accab96c27cabc9addf9e
Emma Collier <emma@exegetic.biz>
2018-05-08 22:35:23 +0200

    Chapter 3 - Null and empty.

--- NULL.php
+++ NULL.php
@@ -3,6 +3,9 @@
 <html lang="en">
 	<head> 
 		<title>NULL</title>
+
+<!-- Things considered empty/not set (therefore null) "" (empty string), null, 0 (number zero), 0.0 (floating zero), false, array() (an empty array) -->
+
 	</head>
 	<body>
 
@@ -14,5 +17,19 @@
 
 	var1 null?: <?php echo is_null($var1); ?><br/>
 	var2 null?: <?php echo is_null($var2); ?><br/>
+	var3 null?: <?php echo is_null($var3); /*Since var3 hasn't been defined it is infact null. Null is the lack of having a value*/?><br/>
+
+	<br/>
+
+	var1 is set?: <?php echo isset($var1); ?><br/>
+	var2 is set?: <?php echo isset($var2); ?><br/>
+	var3 is set?: <?php echo isset($var3); ?><br/>
+
+	<br/>
+
+	<?php $var3 = "0"?>
+	var1 is empty?: <?php echo empty($var1); ?><br/>
+	var2 is empty?: <?php echo empty($var2); ?><br/>
+	var3 is empty?: <?php echo empty($var3); ?><br/>
 	</body>
 </html>
