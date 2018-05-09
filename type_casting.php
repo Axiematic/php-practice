eb42f1171afbe95b56cbc919d0c9c23aae54d4f8
Emma Collier <emma@exegetic.biz>
2018-05-09 11:29:43 +0200

    Chapter 3 - Experimenting with juggling and casting.

--- type_casting.php
+++ type_casting.php
@@ -3,8 +3,42 @@
 <html lang="en">
 	<head> 
 		<title>Type juggling and casting</title>
+		<!-- Type juggling is when php automatically converts one type into another for us without us asking. Type casting is when we ask it to. -->
 	</head>
 	<body>
+
+		<h2>Type Juggling </h2>
+
+			<?php $count = "2";  ?>
+			Type: <?php echo gettype($count); ?><br/>
+
+			<?php $count += 3; /*automatically changes it into an integer for you*/?>
+			Type: <?php echo gettype($count); ?><br/>
+
+			<?php echo $cats = "I have " . $count . " cats.";  ?><br/>
+			Cats: <?php echo gettype($cats); ?><br/>
+
+
+		<br/>
+
+		<h2>Type Casting </h2>	
+		
+			<?php settype($count, "integer"); ?>	
+			count: <?php echo gettype($count); /*settype changes the type INPLACE (permanent)*/ ?><br/>
+
+			<?php $countNew = (string) $count; ?>
+			count: <?php echo gettype($count); ?><br/>
+			countNew: <?php echo gettype($countNew); /*The change takes place only here and isn't permanent*/ ?><br/>
+			<br/>
+
+			<?php $test1 = 3; ?>
+			<?php $test2 = 3; ?>
+
+			<?php settype($test1, "string"); ?>
+			<?php (string) $test2; ?><br/>
+
+			test1: <?php echo gettype($test1); ?><br/>
+			test2: <?php echo gettype($test2); ?><br/>	
 
 	</body>
 </html>
