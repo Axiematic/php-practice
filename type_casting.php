9d0b023820449bf90a3ed4b57167636ff997d7c4
Emma Collier <emma@exegetic.biz>
2018-05-09 10:57:20 +0200

    Chapter 3 - Experimenting with juggling and casting.

---
+++ type_casting.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head> 
+		<title>Type juggling and casting</title>
+	</head>
+	<body>
+
+	</body>
+</html>
