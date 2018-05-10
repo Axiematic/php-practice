824e79f689382ac23de5833c14dfecb1b12766e9
Emma Collier <emma@exegetic.biz>
2018-05-10 11:15:33 +0200

    Basic.html updated and renamed.

---
+++ basic.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head> 
+		<title>untitled</title>
+
+		<?php ?>
+	</head>
+	<body>
+
+	</body>
+</html>
